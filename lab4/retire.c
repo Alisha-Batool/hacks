c28a5697e04893e47773e8d553cb1d4c3d4d702e
Alisha-Batool <144157470+Alisha-Batool@users.noreply.github.com>
2023-10-22 23:11:39 -0700

    declare variable

---
+++ lab4/retire.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  float agi,numChildren,childCredit,tax,totalTax;
+
+
+ printf("Please enter your adjusted gross income (AGI): ");
+  scanf("%lf", &agi);
+  //remove the "enter" endline character
+  getchar(); 
+
+  printf("Do you have any children? (Y) or (N)? ");
+  c = getchar();
+
+  if(c == 'y' || c == 'Y') {
+    printf("How many children do you have? ");
+    scanf("%d", &numChildren);
+  }
+   if (agi <= 20000) {
+    tax = agi * 0.1;
+  } else if (agi <= 50000) {
+    tax = 2000 + (agi - 20000) * 0.15;
+  } else if (agi <= 100000) {
+    tax = 7000 + (agi - 50000) * 0.2;
+  } else {
+    tax = 17000 + (agi - 100000) * 0.25;
+  }
+
+  // Compute the child credit based on the number of children
+  childCredit = numChildren * 1000;
+
+  // Compute the total tax
+  totalTax = tax - childCredit;
+  
+
+  printf("AGI:          $%10.2f\n", agi);
+  printf("Tax:          $%10.2f\n", tax);
+  printf("Child Credit: $%10.2f\n", childCredit);
+  printf("Total Tax:    $%10.2f\n", totalTax);
+
+  return 0;
+}
+
